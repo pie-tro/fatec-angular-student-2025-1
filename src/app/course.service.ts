6ccabd79ae253b58347c30edbc86439eb02ca5c6
pie.tro <pietromaia28@gmail.com>
2025-04-15 11:09:53 -0300

    adicionei courses no json, mudei service e component do courses para mostrar os dados do json na pagina de cursos

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from './course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
+  private apiUrl = 'http://localhost:3000/courses';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.apiUrl);
+  }
 }
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../course.service';
+import { Course } from '../course';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: false,
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.css'
+  styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+  courses: Course[] = [];
+  formGroupCourse: FormGroup;
+
+  constructor(private service: CourseService, private formBuilder: FormBuilder) {
+    this.formGroupCourse = this.formBuilder.group({
+      id: [''],
+      name: [''],
+      price: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.service.getCourses().subscribe({
+      next: json => this.courses = json
+    });
+  }
 }
